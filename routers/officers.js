b1b5cc6c2a05ce55f7271ba2359a0688d1872ad1
ahmad-fajar <shin.fajar@gmail.com>
2017-07-20 22:07:40 +0700

    officers, view all

--- routers/officers.js
+++ routers/officers.js
@@ -1,8 +1,17 @@
 'use strict'
 const router = require('express').Router();
+const db = require('../models');
 
+// index officers, view all
 router.get('/', (req, res) => {
-  res.render('officers')
+  // res.render('allbaby')
+  db.Bayi.findAll({
+    include : [db.Mantri, db.OrangTua],
+    order : [['namabayi', 'ASC']]
+  })
+  .then(data_bayi => {
+    res.render('allbaby', {data_bayi : data_bayi})
+  })
 })
 
 module.exports = router
