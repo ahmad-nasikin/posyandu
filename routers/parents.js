f67a5081b992036c06a835efb10a8b5770148c54
ahmad-fajar <shin.fajar@gmail.com>
2017-07-20 20:41:42 +0700

    waktunya commit

--- routers/parents.js
+++ routers/parents.js
@@ -13,17 +13,46 @@ const db = require('../models');
 //   })
 // })
 
+// index orangtua
+// view bayi dan petugas
 router.get('/:ortuid', (req, res) => {
   db.Bayi.findAll({
     where : {
       OrangTuaId : req.params.ortuid
     },
-    include : db.Mantri
+    include : [db.Mantri, db.OrangTua],
+    order : [['id', 'ASC']]
   })
   .then(data => {
     // console.log(data);
     res.render('parents', {data : data})
   })
 })
+
+
+//tambah data baru
+router.get('/:ortuid/add', (req, res) => {
+  res.send('tes')
+})
+
+
+// detail bayi
+router.get('/:ortuid/:bayiid', (req, res) => {
+  db.Bayi.findById(req.params.bayiid, {include : [db.Mantri, db.OrangTua]})
+  .then(data_bayi => {
+    db.BayiVaksin.findAll({
+      where : {
+        BayiId : req.params.bayiid
+      },
+      include : [db.Vaksin]
+    })
+    .then(data_vaksin => {
+      // console.log(data_vaksin[1]);
+      res.render('detailbaby', {data_bayi : data_bayi, data_vaksin : data_vaksin})
+    })
+  })
+})
+
+
 
 module.exports = router
