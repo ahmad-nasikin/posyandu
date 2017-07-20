be1102e2febe8fdede21cbd1a42d7803be426183
ahmad-fajar <shin.fajar@gmail.com>
2017-07-20 16:05:36 +0700

    isi 4 seeder

--- seeders/seeder - bayi.js
+++ seeders/seeder - bayi.js
@@ -18,61 +18,81 @@ module.exports = {
       ttl : '2014-03-02',
       kelamin : 'Perempuan',
       OrtuId : 1,
-      PetugasId : 2
+      PetugasId : 2,
+      createdAt : new Date(),
+      updatedAt : new Date()
     }, {
       namabayi : 'Rosihan Anwar',
       kelamin : 'Perempuan',
       ttl : '2016-07-18',
       OrtuId : 1,
-      PetugasId : 2
+      PetugasId : 2,
+      createdAt : new Date(),
+      updatedAt : new Date()
     }, {
       namabayi : 'I Gak Chatur',
       kelamin : 'Laki-laki',
       ttl : '2017-05-01',
       OrtuId : 2,
-      PetugasId :1
+      PetugasId :1,
+      createdAt : new Date(),
+      updatedAt : new Date()
     }, {
       namabayi : 'Muhammad Irfan',
       kelamin : 'Laki-laki',
       ttl : '2015-27-25',
       OrtuId : 3,
-      PetugasId : 2
+      PetugasId : 2,
+      createdAt : new Date(),
+      updatedAt : new Date()
     }, {
       namabayi : 'Tri Rachmanto',
       kelamin : 'Laki-laki',
       ttl : '2015-12-05',
       OrtuId : 4,
-      PetugasId : 3
+      PetugasId : 3,
+      createdAt : new Date(),
+      updatedAt : new Date()
     }, {
       namabayi : 'Nurpatria',
       kelamin : 'Laki-laki',
       ttl : '2016-11-15',
       OrtuId : 4,
-      PetugasId : 3
+      PetugasId : 3,
+      createdAt : new Date(),
+      updatedAt : new Date()
     }, {
       namabayi : 'Muhammad Zia Ulhaq',
       kelamin : 'Laki-laki',
       ttl : '2017-04-01',
       OrtuId : 5,
-      PetugasId : 3
+      PetugasId : 3,
+      createdAt : new Date(),
+      updatedAt : new Date()
     }, {
       namabayi : 'Nurhasanah',
       kelamin : 'Perempuan',
       ttl : '2015-09-23',
       OrtuId : 6,
-      PetugasId : 2
+      PetugasId : 2,
+      createdAt : new Date(),
+      updatedAt : new Date()
     }, {
       namabayi : 'Riza Hidayat',
       kelamin : 'Laki-laki',
       ttl : '2017-02-13',
       OrtuId : 6,
-      PetugasId : 1
+      PetugasId : 1,
+      createdAt : new Date(),
+      updatedAt : new Date()
     }, {
       namabayi : 'Ridho Pandu Dirgantara',
       kelamin : 'Laki-laki',
       ttl : '2013-12-16',
       OrtuId : 7,
-      PetugasId : 1
+      PetugasId : 1,
+      createdAt : new Date(),
+      updatedAt : new Date()
     }], {})
   },
 
